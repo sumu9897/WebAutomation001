151a3312513d0bb1ff8889bbe8a6b3674da1f408
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2023-12-20 20:37:46 +0600

    First WebAutomation Testing

---
+++ src/test/java/org/example/OpenAWebPage.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class OpenAWebPage {
+//    public static void main (String[] args){
+//        WebDriver browser = new ChromeDriver();
+//        browser.get("https://github.com/sumu9897?tab=repositories");
+//    }
+    @Test
+    public void openAPage(){
+
+        WebDriver browser = new FirefoxDriver();
+        browser.get("https://github.com/sumu9897?tab=repositories");
+
+    }
+}
