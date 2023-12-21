e859cfb514452b7781c4e1790acddeea5b74f0a4
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2023-12-21 08:59:14 +0600

    [DONE] Small Update

--- src/test/java/org/example/OpenAWebPage.java
+++ src/test/java/org/example/OpenAWebPage.java
@@ -3,6 +3,7 @@ package org.example;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class OpenAWebPage {
@@ -14,7 +15,9 @@ public class OpenAWebPage {
     public void openAPage(){
 
         WebDriver browser = new FirefoxDriver();
-        browser.get("https://github.com/sumu9897?tab=repositories");
+        browser.get("https://www.toptenmartltd.com/");
 
+        browser.close();
     }
+
 }
